b089619c6c6fcc6c7163dd91b845e36e0797f446
Randall Degges <r@rdegges.com>
2017-10-01 01:56:48 -0700

    Adding more tests for Verifier object.

--- test/index.js
+++ test/index.js
@@ -1,5 +1,12 @@
+"use strict";
+
 const assert = require("assert");
 const Verifier = require("..");
+
+
+const USERNAME = process.env.WHOIS_USERNAME;
+const PASSWORD = process.env.WHOIS_PASSWORD;
+
 
 describe("Verifier", () => {
   describe("#verifyOptions()", () => {
@@ -23,4 +30,30 @@ describe("Verifier", () => {
       assert.throws(() => { new Verifier("username", "password", { retries: "retries" }); }, "opts.retries must be a number");
     });
   });
+
+  describe("#verify()", () => {
+    it("should return an error if the username/password are invalid", (done) => {
+      let verifier = new Verifier("as;ljfdsagsag", "asgddsafdsakjldsa");
+
+      verifier.verify("r@rdegges.com", (err, resp) => {
+        assert(typeof err.message === "string");
+        done();
+      });
+    });
+
+    it("should return a response", (done) => {
+      let verifier = new Verifier(USERNAME, PASSWORD);
+
+      verifier.verify("r@rdegges.com", (err, resp) => {
+        assert.ifError(err);
+        assert(resp && resp.EmailVerifyRecord);
+        assert.equal(resp.EmailVerifyRecord.emailAddress, "r@rdegges.com");
+        done();
+      });
+    });
+  });
 });
+
+if (!(USERNAME && PASSWORD)) {
+  throw new Error("WHOIS_USERNAME and WHOIS_PASSWORD environment variables not set.");
+}
